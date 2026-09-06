454aab1e5c89334148502b8ea042637e65463da6
samira5-droid <samira.schneider2005@gmail.com>
2026-09-06 14:16:16 +0200

    Restore story data for Pages deployment

---
+++ story-data.js
@@ -0,0 +1,3 @@
+window.STORY_PAGES = [];
+// Full story corpus is being restored from Hogwarts.pdf in the next update.
+window.EPISODE_THREE = {scenes:[{loc:'Ravenclaw Tower',text:'Morning arrives too quickly.',choices:[{label:'Continue.',next:0}]}]};
